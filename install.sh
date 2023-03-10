a0f9f7f43e69823d9eeaa0152af6ad7b5e69bc0e
Sami J. Mäkinen <sjm@iki.fi>
2023-03-10 23:50:05 +0200

    oops

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export PATH
 
 tgt=$HOME/coap-server/bin
-rsync -var target/release/coap-server $tgt/
+rsync -var target/release/coap_server_temp $tgt/
 
 exit 0
 # EOF
